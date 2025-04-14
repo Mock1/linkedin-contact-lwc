dc636ed46e975fc3a81486021c871fdd8afee3ec
MICHAEL MOCK <michaelrmock@gmail.com>
2025-04-14 12:05:11 -0500

    Initial prompt
    Ok, now let's create an LWC. I want an LWC that will go on the Contact record page. I should display the name, title, company name (and hyperlink to the Account), and the avatar picture, a URL stored in Avatar__c on the Contact record. This should fit in style with the Salesforce Lightning Design System, and be similar to a LinkedIn profile card, as the function will be the same, to show a graphical, immediate representation of the Contact.

---
+++ force-app/main/default/lwc/contactProfileCard/contactProfileCard.js
@@ -0,0 +1,41 @@
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import NAME_FIELD from '@salesforce/schema/Contact.Name';
+import TITLE_FIELD from '@salesforce/schema/Contact.Title';
+import AVATAR_FIELD from '@salesforce/schema/Contact.Avatar__c';
+import ACCOUNT_FIELD from '@salesforce/schema/Contact.Account';
+import ACCOUNT_NAME_FIELD from '@salesforce/schema/Contact.Account.Name';
+
+const FIELDS = [
+    NAME_FIELD,
+    TITLE_FIELD,
+    AVATAR_FIELD,
+    ACCOUNT_FIELD,
+    ACCOUNT_NAME_FIELD
+];
+
+export default class ContactProfileCard extends LightningElement {
+    @api recordId;
+    
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    contact;
+
+    get name() {
+        return getFieldValue(this.contact.data, NAME_FIELD);
+    }
+
+    get title() {
+        return getFieldValue(this.contact.data, TITLE_FIELD);
+    }
+
+    get avatar() {
+        return getFieldValue(this.contact.data, AVATAR_FIELD);
+    }
+
+    get account() {
+        return {
+            Id: getFieldValue(this.contact.data, ACCOUNT_FIELD),
+            Name: getFieldValue(this.contact.data, ACCOUNT_NAME_FIELD)
+        };
+    }
+} 
